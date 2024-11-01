a14f1bc29f7b5e2a6518fda3935ccaca2b390ffa
Gurramgreeshma <greeshma2562005@gmail.com>
2024-11-01 10:24:27 +0530

    my about us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,46 @@
-// import React from 'react'
-import './about.css'
+import React from 'react';
+
+
+
+
+
+
+import './about.css';
+
+import chickenFriedRice from '../../assets/chicken-biryani.jpg';
+
 const About = () => {
   return (
-    <div> this is About</div>
-  )
-}
+    
 
-export default About
+    <div className="about-container" >
+      
+      <center>
+        <h1>We Deliver Best & 
+          <br></br>Delicious Food For You</h1>
+      </center>
+      
+      <div id="images-div">
+        <div >
+          <p>Enjoy our food experience</p>
+          <p>We don't just make food,</p>
+          <p>We make people's choices.</p>
+          <h2>100+</h2>
+          <p>Delicious foods in</p>
+          <p>our menu</p>
+        </div>
+        
+       
+        <div>
+          <img src={chickenFriedRice} alt="Chicken biryani.jpg" />
+        </div>
+      </div> 
+      <div className="button-container">
+  <button className="menu-button">View Our Menu</button>
+</div>
+
+    </div>
+  );
+};
+
+export default About;
